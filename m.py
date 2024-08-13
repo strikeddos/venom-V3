8e9f575ed3d7d0797843a53bee6cf2e3db79b69e
strikeddos <201702sha@gmail.com>
2024-08-13 22:11:52 +0530

    Update m.py

--- m.py
+++ m.py
@@ -10,9 +10,9 @@ from keep_alive import keep_alive
 keep_alive()
 
 # Insert your Telegram bot token here
-bot = telebot.TeleBot('TOKEM')
+bot = telebot.TeleBot('7401399678:AAEvy-_HyllkAizz909salXri26TBuiTLJs')
 # Admin user IDs
-admin_id = {"5588464519"}
+admin_id = {"1359272665"}
 
 # Files for data storage
 USER_FILE = "users.json"
